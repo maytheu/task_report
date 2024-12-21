1c6c876f753970f16c3bf40bf3eaa1f4be3354ea
maytheu <maytheu98@gmail.com>
2024-12-21 02:25:21 +0100

    page layout

--- client/src/app/page/home/home.component.ts
+++ client/src/app/page/home/home.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { ProfileComponent } from '../../component/profile/profile.component';
+import { TasksComponent } from '../../component/tasks/tasks.component';
+import { TaskFormComponent } from "../../component/task-form/task-form.component";
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [ProfileComponent, TasksComponent, TaskFormComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
--- client/src/app/page/task/task.component.ts
+++ client/src/app/page/task/task.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { TaskFormComponent } from "../../component/task-form/task-form.component";
 
 @Component({
   selector: 'app-task',
-  imports: [],
+  imports: [TaskFormComponent],
   templateUrl: './task.component.html',
   styleUrl: './task.component.css'
 })
